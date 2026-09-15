0593a0ab207e9f5d7877c13727c4663ff3189433
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-09-15 10:37:56 +0600

    d

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const NavBar = () => {
           <img className="w-10" src="/logo.png" /> Ai Hub
         </div>
       </div>
+
       <div className="navbar-center hidden md:flex">
         <ul className="menu menu-horizontal gap-10 px-1 text-lg">
           <li>
@@ -22,6 +23,7 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
+
       <div className="navbar-end gap-5">
         <a className="btn bg-red-500 rounded-full text-white">Get in Touch</a>
       </div>
