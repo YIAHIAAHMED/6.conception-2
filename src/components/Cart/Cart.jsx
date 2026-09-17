0aa390950a036e2f5ad665117f14f47095ee3505
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-09-17 19:34:04 +0600

    g

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,33 +1,45 @@
 import React from 'react';
 
-const Cart = ({ carts }) => {
+const Cart = ({ carts, setCarts }) => {
+    const totalPrice = carts.reduce((sum, item) => sum + item.price, 0)
+    // console.log(totalPrice);
+    const handlePayment = () => {
+        setCarts([])
+    }
     return (
         <div>
             <h1 className='text-2xl font-bold'>Yours Carts</h1>
             {
-                carts.map(item =>
-                    <div className="space-y-3 mt-4">
-                        <div key={item.id} className='flex justify-between items-center border'>
-                            <div>
-                                <img className='h-20 w-20 object-contain' src={item.image} />
-                            </div>
-                            <div className="text-2xl font-bold">
-                                <h2>{item.title} </h2>
-                            </div>
-                            <div className="text-3xl font-bold">
-                                <p>${item.price}/month</p>
-                            </div>
+                carts.length === 0 ? <p>Your Cart is empty</p> :
+                    <>
+                        {
+                            carts.map(item =>
+                                <div className="space-y-3 mt-4">
+                                    <div key={item.id} className='flex justify-between items-center border'>
+                                        <div>
+                                            <img className='h-20 w-20 object-contain' src={item.image} />
+                                        </div>
+                                        <div className="text-2xl font-bold">
+                                            <h2>{item.title} </h2>
+                                        </div>
+                                        <div className="text-3xl font-bold">
+                                            <p>${item.price}/month</p>
+                                        </div>
 
+                                    </div>
+                                </div>
+                            )
+                        }
+                        <div className="flex justify-between bg-amber-600 mt-4 p-5 text-3xl font-bold ">
+                            <div className="">Total</div>
+                            <div className="">${totalPrice}/month </div>
                         </div>
-                    </div>
-                )
+                        <button onClick={handlePayment} className='btn w-full mt-5 bg-amber-500 text-white text-2xl rounded-lg'>Proceed to Checkout</button>
+                    </>
             }
-            <div className="flex justify-between bg-amber-600 mt-4 ">
-                <div className="">Total</div>
-                <div className="">0</div>
-            </div>
+
         </div>
-        
+
     );
 };
 
